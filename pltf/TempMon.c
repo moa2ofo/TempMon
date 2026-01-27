31d87bcfff2e7e10b97e790c463b8205eb74e3ae
Andrea Monti <131896606+moa2ofo@users.noreply.github.com>
2026-01-27 17:52:16 +0100

    unit has been auto generated

--- pltf/TempMon.c
+++ pltf/TempMon.c
@@ -39,29 +39,46 @@ void TempMon_Init(int32_t temp_mC) {
   TempMon_Run(temp_mC);
 }
 
-void TempMon_Run(int32_t temp_mC) {
-  if (TempMon_sts_e == NORMAL) {
-    if (temp_mC < g_UnderThreshold_mC_s32) {
-      TempMon_sts_e = UNDER;
-    } else if (temp_mC > g_OverThreshold_mC_s32) {
-      TempMon_sts_e = OVER;
-    } else {
-      /* Remain NORMAL */
+void TempMon_Run(int32_t temp_mC)
+{
+    switch (TempMon_sts_e)
+    {
+        case NORMAL:
+            /* Check if temperature is below under threshold */
+            if (temp_mC < g_UnderThreshold_mC_s32)
+            {
+                TempMon_sts_e = UNDER;
+            }
+            /* Check if temperature is above over threshold */
+            else if (temp_mC > g_OverThreshold_mC_s32)
+            {
+                TempMon_sts_e = OVER;
+            }
+            /* else remain NORMAL */
+            break;
+
+        case UNDER:
+            /* Check if temperature has risen above under threshold plus hysteresis */
+            if (temp_mC > (g_UnderThreshold_mC_s32 + g_Hyst_mC_s32))
+            {
+                TempMon_sts_e = NORMAL;
+            }
+            /* else remain UNDER */
+            break;
+
+        case OVER:
+            /* Check if temperature has fallen below over threshold minus hysteresis */
+            if (temp_mC < (g_OverThreshold_mC_s32 - g_Hyst_mC_s32))
+            {
+                TempMon_sts_e = NORMAL;
+            }
+            /* else remain OVER */
+            break;
+
+        default:
+            /* No action for undefined states */
+            break;
     }
-  } else if (TempMon_sts_e == UNDER) {
-    if (temp_mC > (g_UnderThreshold_mC_s32 + g_Hyst_mC_s32)) {
-      TempMon_sts_e = NORMAL;
-    } else {
-      /* Remain UNDER */
-    }
-  } else /* TempMon_sts_e == OVER */
-  {
-    if (temp_mC < (g_OverThreshold_mC_s32 - g_Hyst_mC_s32)) {
-      TempMon_sts_e = NORMAL;
-    } else {
-      /* Remain OVER */
-    }
-  }
 }
 TempMon_sts_e TempMon_GetSts(void) { return Sts_e; }
 
