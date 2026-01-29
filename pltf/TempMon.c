87e00efc49b3e07b4f6a80dc835320500e7579a8
Andrea Monti <131896606+moa2ofo@users.noreply.github.com>
2026-01-29 10:26:30 +0100

    unit has been auto generated

--- pltf/TempMon.c
+++ pltf/TempMon.c
@@ -31,58 +31,36 @@ void TempMon_Init(int32_t temp_mC) {
   TempMon_Run(temp_mC);
 }
 
-void TempMon_Run(const int32_t temp_mC) {
-  /* Start of TempMon_Run function */
-
-  /* Update state machine based on current state and temperature */
-
-  switch(Sts_e) {
-  case TEMPMON_STS_NORMAL:
-    /* If temperature below under threshold, enter UNDER state */
-    if(temp_mC < g_UnderThreshold_mC_s32) {
-      Sts_e = TEMPMON_STS_UNDER;
+void TempMon_Run(int32_t temp_mC)
+{
+    if (Sts_e == TEMPMON_STS_NORMAL)
+    {
+        if (temp_mC < g_UnderThreshold_mC_s32)
+        {
+            Sts_e = TEMPMON_STS_UNDER;
+        }
+        else if (temp_mC > g_OverThreshold_mC_s32)
+        {
+            Sts_e = TEMPMON_STS_OVER;
+        }
+        /* else remain TEMPMON_STS_NORMAL */
     }
-    /* Else if temperature above over threshold, enter OVER state */
-    else if(temp_mC > g_OverThreshold_mC_s32) {
-      Sts_e = TEMPMON_STS_OVER;
+    else if (Sts_e == TEMPMON_STS_UNDER)
+    {
+        if (temp_mC > (g_UnderThreshold_mC_s32 + g_Hyst_mC_s32))
+        {
+            Sts_e = TEMPMON_STS_NORMAL;
+        }
+        /* else remain TEMPMON_STS_UNDER */
     }
-    /* Else remain in NORMAL state */
-    else {
-      /* Remain in TEMPMON_STS_NORMAL */
+    else /* Sts_e == TEMPMON_STS_OVER */
+    {
+        if (temp_mC < (g_OverThreshold_mC_s32 - g_Hyst_mC_s32))
+        {
+            Sts_e = TEMPMON_STS_NORMAL;
+        }
+        /* else remain TEMPMON_STS_OVER */
     }
-    break;
-
-  case TEMPMON_STS_UNDER:
-    /* If temperature above under threshold plus hysteresis, return to NORMAL */
-    if(temp_mC > (g_UnderThreshold_mC_s32 + g_Hyst_mC_s32)) {
-      Sts_e = TEMPMON_STS_NORMAL;
-    }
-    /* Else remain in UNDER state */
-    else {
-      /* Remain in TEMPMON_STS_UNDER */
-    }
-    break;
-
-  case TEMPMON_STS_OVER:
-    /* If temperature below over threshold minus hysteresis, return to NORMAL */
-    if(temp_mC < (g_OverThreshold_mC_s32 - g_Hyst_mC_s32)) {
-      Sts_e = TEMPMON_STS_NORMAL;
-    }
-    /* Else remain in OVER state */
-    else {
-      /* Remain in TEMPMON_STS_OVER */
-    }
-    break;
-
-  default:
-    /**
-     * @brief No action for undefined states.
-     * This default case ensures stability if Sts_e has an unexpected value.
-     */
-    break;
-  }
-
-  /* End of TempMon_Run function */
 }
 TempMon_sts_e TempMon_GetSts(void) { return Sts_e; }
 
