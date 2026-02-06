0eeb753b6e86e3e254b695e7e7c76a921e932173
Andrea Monti <Andrea.Monti@vhit-weifu.com>
2026-02-06 14:26:33 +0100

    correct the decsription

--- pltf/TempMon.h
+++ pltf/TempMon.h
@@ -163,37 +163,16 @@ void TempMon_Init(int32_t temp_mC);
  * @details
  * **Goal of the function**
  *
- * Update the internal state machine based on the latest temperature sample.
- *
- * The processing logic:
- * - If current state is TEMPMON_STS_NORMAL:
- *   - Enter TEMPMON_STS_UNDER if `temp_mC < g_UnderThreshold_mC_s32`.
- *   - Else enter TEMPMON_STS_OVER if `temp_mC > g_OverThreshold_mC_s32`.
- *   - Else remain TEMPMON_STS_NORMAL.
- * - If current state is TEMPMON_STS_UNDER:
- *   - Return to TEMPMON_STS_NORMAL if `temp_mC > (g_UnderThreshold_mC_s32 +
- *     g_Hyst_mC_s32)`.
- *   - Else remain TEMPMON_STS_UNDER.
- * - If current state is TEMPMON_STS_OVER:
- *   - Return to TEMPMON_STS_NORMAL if `temp_mC < (g_OverThreshold_mC_s32 -
- *     g_Hyst_mC_s32)`.
- *   - Else remain TEMPMON_STS_OVER.
  *
  * @par Interface summary
  *
- * | Interface                 | In | Out | Data type / Signature    | Param |
- * Data factor | Data offset | Data size | Data range | Data unit |
- * |--------------------------|:--:|:---:|--------------------------|:-----:|------------:|------------:|----------:|------------------------------------------------------|----------|
- * | temp_mC                  | X  |     | int32_t                  |   X   | 1
- * |           0 |         1 | implementation-defined | [mdegC]  | |
- * g_UnderThreshold_mC_s32  | X  |     | int32_t (extern)         |   -   | 1 |
- * 0 |         1 | implementation-defined                               |
- * [mdegC]  | | g_OverThreshold_mC_s32   | X  |     | int32_t (extern)         |
- * -   |           1 |           0 |         1 | implementation-defined |
- * [mdegC]  | | g_Hyst_mC_s32            | X  |     | int32_t (extern)         |
- * -   |           1 |           0 |         1 | typically >= 0 | [mdegC]  | |
- * Sts_e                    | X  |  X  | TempMon_sts_e (static)   |   -   | - |
- * - |         - | [TEMPMON_STS_NORMAL / TEMPMON_STS_UNDER / _STS_OVER] | [-] |
+ * | Interface               | In | Out | Data type / Signature     | Param | Data factor | Data offset | Data size | Data range                                           | Unit   |
+ * |------------------------|:--:|:---:|----------------------------|:-----:|------------:|------------:|----------:|------------------------------------------------------|--------|
+ * | temp_mC                | X  |     | int32_t                    | X     | 1           | 0           | 1         | implementation-defined                                | mdegC  |
+ * | g_UnderThreshold_mC_s32| X  |     | int32_t (extern)           | -     | 1           | 0           | 1         | implementation-defined                                | mdegC  |
+ * | g_OverThreshold_mC_s32 | X  |     | int32_t (extern)           | -     | 1           | 0           | 1         | implementation-defined                                | mdegC  |
+ * | g_Hyst_mC_s32          | X  |     | int32_t (extern)           | -     | 1           | 0           | 1         | typically >= 0                                        | mdegC  |
+ * | Sts_e                  | X  |  X  | TempMon_sts_e (static)     | -     | -           | -           | -         | TEMPMON_STS_NORMAL / _UNDER / _OVER                   | -      |
  *
  * @par Activity diagram (PlantUML)
  *
--- unitTest/TEST_TempMon_Run/src/TempMon_Run.h
+++ unitTest/TEST_TempMon_Run/src/TempMon_Run.h
@@ -7,31 +7,52 @@
  * @brief Run one update step of the temperature monitor.
  *
  * @details
- * Update the internal state machine based on the latest temperature sample.
+ * **Goal of the function**
  *
- * Processing logic:
- * - If current state is TEMPMON_STS_NORMAL:
- *   - Enter TEMPMON_STS_UNDER if `temp_mC < g_UnderThreshold_mC_s32`.
- *   - Else enter TEMPMON_STS_OVER if `temp_mC > g_OverThreshold_mC_s32`.
- *   - Else remain TEMPMON_STS_NORMAL.
- * - If current state is TEMPMON_STS_UNDER:
- *   - Return to TEMPMON_STS_NORMAL if `temp_mC > (g_UnderThreshold_mC_s32 + g_Hyst_mC_s32)`.
- *   - Else remain TEMPMON_STS_UNDER.
- * - If current state is TEMPMON_STS_OVER:
- *   - Return to TEMPMON_STS_NORMAL if `temp_mC < (g_OverThreshold_mC_s32 - g_Hyst_mC_s32)`.
- *   - Else remain TEMPMON_STS_OVER.
  *
  * @par Interface summary
  *
- * | Interface               | In | Out | Data type / Signature     | Param | Data factor | Data offset | Data size | Data range                                           | Unit   |
- * |------------------------|:--:|:---:|----------------------------|:-----:|------------:|------------:|----------:|------------------------------------------------------|--------|
- * | temp_mC                | X  |     | int32_t                    | X     | 1           | 0           | 1         | implementation-defined                                | mdegC  |
- * | g_UnderThreshold_mC_s32| X  |     | int32_t (extern)           | -     | 1           | 0           | 1         | implementation-defined                                | mdegC  |
- * | g_OverThreshold_mC_s32 | X  |     | int32_t (extern)           | -     | 1           | 0           | 1         | implementation-defined                                | mdegC  |
- * | g_Hyst_mC_s32          | X  |     | int32_t (extern)           | -     | 1           | 0           | 1         | typically >= 0                                        | mdegC  |
- * | Sts_e                  | X  |  X  | TempMon_sts_e (static)     | -     | -           | -           | -         | TEMPMON_STS_NORMAL / _UNDER / _OVER                   | -      |
+ * | Interface               | In | Out | Data type / Signature     | Param | Data factor | Data offset | Data size | Data range                          | Unit   |
+ * |------------------------|:--:|:---:|----------------------------|:-----:|------------:|------------:|----------:|-------------------------------------|--------|
+ * | temp_mC                | X  |     | int32_t                    | X     | 1           | 0           | 1         | [SINT32_MIN,SINT32_MAX]             | mdegC  |
+ * | g_UnderThreshold_mC_s32| X  |     | int32_t (extern)           | -     | 1           | 0           | 1         | [SINT32_MIN,SINT32_MAX]             | mdegC  |
+ * | g_OverThreshold_mC_s32 | X  |     | int32_t (extern)           | -     | 1           | 0           | 1         | [SINT32_MIN,SINT32_MAX]             | mdegC  |
+ * | g_Hyst_mC_s32          | X  |     | int32_t (extern)           | -     | 1           | 0           | 1         | [SINT32_MIN,SINT32_MAX]             | mdegC  |
+ * | Sts_e                  | X  |  X  | TempMon_sts_e (static)     | -     | 1           | 0           | 1         | TEMPMON_STS_NORMAL / _UNDER / _OVER | -      |
  *
- * @param temp_mC Current temperature in milli-degC.
+ * @par Activity diagram (PlantUML)
+ *
+ * @startuml
+ * start
+ * if (Sts_e == TEMPMON_STS_NORMAL) then (yes)
+ *   if (temp_mC <  g_UnderThreshold_mC_s32) then (yes)
+ *     :Sts_e = TEMPMON_STS_UNDER;
+ *   else (no)
+ *     if (temp_mC > g_OverThreshold_mC_s32) then (yes)
+ *       :Sts_e = TEMPMON_STS_OVER;
+ *     else (no)
+ *       :stay TEMPMON_STS_NORMAL;
+ *     endif
+ *   endif
+ * elseif (Sts_e == TEMPMON_STS_UNDER) then (yes)
+ *   if (temp_mC >  g_UnderThreshold_mC_s32 + g_Hyst_mC_s32) then (yes)
+ *     :Sts_e = TEMPMON_STS_NORMAL;
+ *   else (no)
+ *     :stay TEMPMON_STS_UNDER;
+ *   endif
+ * else (TEMPMON_STS_OVER)
+ *   if (temp_mC < g_OverThreshold_mC_s32 - g_Hyst_mC_s32) then (yes)
+ *     :Sts_e = TEMPMON_STS_NORMAL;
+ *   else (no)
+ *     :stay TEMPMON_STS_OVER;
+ *   endif
+ * endif
+ * stop
+ * @enduml
+ *
+ * @param temp_mC
+ * Current temperature in milli-degC (mdegC).
+ *
  * @return void
  */
 void TempMon_Run(int32_t temp_mC);
