fd06869119f69ac6d0c063015e2b021eee91a36f
Andrea Monti <Andrea.Monti@vhit-weifu.com>
2026-03-04 15:27:35 +0100

    delete unit tests

--- unitTest/TEST_IsOverExit_b/src/IsOverExit_b.h
+++
@@ -1,9 +0,0 @@
-#ifndef TEST_ISOVEREXIT_B_H
-#define TEST_ISOVEREXIT_B_H
-
-#include "TempMon.h"
-#include "TempMon_priv.h"
-
-_Bool IsOverExit_b(int32_t temp_mC);
-
-#endif /* TEST_ISOVEREXIT_B_H */
